d12e78dc6030ed0fdbdf8cb2603faf64cd0c23bd
Francesco Zanini <francesco@zanini.me>
2018-12-21 21:11:19 +0100

    Update unifi-controller to 5.9.29
    - Split volumes
    - Improve entrypoint script
    - Run as "unifi" instead of root

--- unifi-controller/docker-entrypoint.sh
+++ unifi-controller/docker-entrypoint.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-# create our folders
-mkdir -p /data/{data,logs,run}
+if [ "$1" = 'unifi' ]; then
+    # create our folders
+    mkdir -p /app/run
 
-# create symlinks for data
-[[ -L /usr/lib/unifi/data && ! /usr/lib/unifi/data -ef /data/data ]] && unlink /usr/lib/unifi/data
-[[ -L /usr/lib/unifi/logs && ! /usr/lib/unifi/logs -ef /data/logs ]] && unlink /usr/lib/unifi/logs
-[[ -L /usr/lib/unifi/run && ! /usr/lib/unifi/run -ef /data/run ]] && unlink /usr/lib/unifi/run
-[[ ! -L /usr/lib/unifi/data ]] && ln -s /data/data /usr/lib/unifi/data
-[[ ! -L /usr/lib/unifi/logs ]] && ln -s /data/logs /usr/lib/unifi/logs
-[[ ! -L /usr/lib/unifi/run ]] && ln -s /data/run /usr/lib/unifi/run
+    # create symlinks for data
+    [[ -L /usr/lib/unifi/data && ! /usr/lib/unifi/data -ef /app/data ]] && unlink /usr/lib/unifi/data
+    [[ -L /usr/lib/unifi/logs && ! /usr/lib/unifi/logs -ef /app/logs ]] && unlink /usr/lib/unifi/logs
+    [[ -L /usr/lib/unifi/run && ! /usr/lib/unifi/run -ef /app/run ]] && unlink /usr/lib/unifi/run
+    [[ ! -L /usr/lib/unifi/data ]] && ln -s /app/data /usr/lib/unifi/data
+    [[ ! -L /usr/lib/unifi/logs ]] && ln -s /app/logs /usr/lib/unifi/logs
+    [[ ! -L /usr/lib/unifi/run ]] && ln -s /app/run /usr/lib/unifi/run
 
-exec /usr/bin/java ${JVM_OPTS} -jar /usr/lib/unifi/lib/ace.jar "$@"
+    chown -R unifi /usr/lib/unifi /app
+
+    exec gosu unifi /usr/bin/java ${JVM_OPTS} -jar /usr/lib/unifi/lib/ace.jar start
+fi
+
+exec "$@"
