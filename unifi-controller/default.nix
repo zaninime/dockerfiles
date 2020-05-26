c9a7673a95f9d3cfb7f75fc2f007a16da270e70e
Francesco Zanini <francesco@zanini.me>
2020-05-26 18:11:31 +0200

    unifi: 5.12.66 -> 5.12.72, drop 5.6

--- unifi-controller/default.nix
+++ unifi-controller/default.nix
@@ -6,17 +6,8 @@ let
 in rec {
   unifi-5_12_x = callPackage ./docker.nix {
     inherit imagePrefix;
-    src = pkgs.unifi-5-12;
+    src = pkgs.unifi-5_12_x;
   };
 
   unifi-5_12 = unifi-5_12_x.override { imageTag = "5.12"; };
-
-  # ---
-
-  unifi-5_6_x = callPackage ./docker.nix {
-    inherit imagePrefix;
-    src = pkgs.unifi-5-6;
-  };
-
-  unifi-5_6 = unifi-5_6_x.override { imageTag = "5.6"; };
 }
